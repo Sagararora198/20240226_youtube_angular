cb764b3c44bc55caa6d165ad031edfb183453919
Sagar <sagar.arora198@gmail.com>
2024-02-26 16:10:13 +0530

    routes defined

--- youtube_frontend/youtube-app/src/app/app.routes.ts
+++ youtube_frontend/youtube-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomepageComponent
+    },
+    {
+        path:'/setting',
+        
+    },
+    {
+        path:'/subscription',
+    }
+];
