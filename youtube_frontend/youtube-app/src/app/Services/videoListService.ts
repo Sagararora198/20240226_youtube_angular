83eecee279429e050ad504805ad1a1f334591ce7
Sagar <sagar.arora198@gmail.com>
2024-02-27 11:44:47 +0530

    setting added

--- youtube_frontend/youtube-app/src/app/Services/videoListService.ts
+++ youtube_frontend/youtube-app/src/app/Services/videoListService.ts
@@ -5,55 +5,55 @@
 export class VideoList  {
     Videos:{video_name:string,video_preview:string,video_desc:string,subscribed:boolean}[]=[
         {
-          video_name:"this",
+          video_name:"Lorem ipsum dolor sit amet",
           video_preview:"../../assets/n.png",
           video_desc:"",
           subscribed:true
         },
         {
-          video_name:"this",
-          video_preview:"../../assets/n.png",
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/50-536x354.jpg",
           video_desc:"",
           subscribed:false
         },
         {
-          video_name:"this",
-          video_preview:"../../assets/n.png",
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/384-536x354.jpg",
+          video_desc:"",
+          subscribed:false
+        },
+        {
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/564-536x354.jpg",
           video_desc:"",
           subscribed:true
         },
         {
-          video_name:"this",
-          video_preview:"sef",
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/915-536x354.jpg",
           video_desc:"",
           subscribed:true
         },
         {
-          video_name:"this",
-          video_preview:"sef",
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/995-536x354.jpg",
           video_desc:"",
           subscribed:true
         },
         {
-          video_name:"this",
-          video_preview:"sef",
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/1018-536x354.jpg",
           video_desc:"",
           subscribed:true
         },
         {
-          video_name:"this",
-          video_preview:"sef",
+          video_name:"Lorem ipsum dolor sit amet",
+          video_preview:"../../assets/1038-536x354.jpg",
           video_desc:"",
           subscribed:true
         },
         {
-          video_name:"this",
-          video_preview:"sef",
-          video_desc:"",
-          subscribed:true
-        },
-        {
-          video_name:"this",
+          video_name:"Lorem ipsum dolor sit amet",
           video_preview:"sef",
           video_desc:"",
           subscribed:true
--- youtube_frontend/youtube-app/src/app/setting/setting.component.ts
+++ youtube_frontend/youtube-app/src/app/setting/setting.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-setting',
-  standalone: true,
-  imports: [],
-  templateUrl: './setting.component.html',
-  styleUrl: './setting.component.css'
+    selector: 'app-setting',
+    standalone: true,
+    templateUrl: './setting.component.html',
+    styleUrl: './setting.component.css',
+    imports: [NavbarComponent]
 })
 export class SettingComponent {
 
